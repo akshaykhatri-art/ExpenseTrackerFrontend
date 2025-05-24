1acf2660329c666a03a8d0f766d8395cca7c49ef
Akshay <akshaykhatri22@gmail.com>
2025-05-25 03:25:13 +0530

    Basic Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Expense Tracker Application</h1>
+      <p className="read-the-docs">loading...</p>
+    </>
+  );
+}
+
+export default App;
